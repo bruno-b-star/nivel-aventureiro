b6fa50edb469b09e009b6b29137f1a46fe0cad4d
bruno-b-star <bernardobruno156@gmail.com>
2025-04-09 22:36:08 +0000

    exercicios

---
+++ pratica-formulas-matematicas.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+
+        int dia1, dia2, dia3, dia4, dia5;
+        int total, media, aumento_total, total_corrigido, por_caminhao;
+
+        printf("Digite a produção do dia 1: \n");
+        scanf("%d\n", &dia1);
+
+        printf("Digite a produção do dia 2: \n");
+        scanf("%d\n", &dia2);
+
+        printf("Digite a produção do dia 3: \n");
+        scanf("%d\n", &dia3);
+
+        printf("Digite a produção do dia 4: \n");
+        scanf("%d\n", &dia4);
+
+        printf("Digite a produção do dia 5: \n");
+        scanf("%d\n", &dia5);
+
+        total = dia1 + dia2 + dia3 + dia4 + dia5;
+        media = total/ 5;
+
+        aumento_total = (dia1)
+
+
+}
